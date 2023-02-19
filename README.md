2862593488e092fead666e5a2ea0e63c418f64ab
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-02-19 11:06:57 -0700

    layout architecture in README; add initial client and server

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # zk-autobattler
+
+## Architecture
+- `client/`
+    - React JS app
+    - Handles all the game logic that does not need to be verified
+    - Talks to API to generate proofs and get values from DB
+    - Handles storage of user creations
+- `server/`
+    - Rust backend API
+    - Endpoints
+        - `/games/join` (POST)
+            - Input: optional_lobby_id
+            - Join's a random lobby ID or creates a new one
+        - `/games/play` (POST)
+            - Input: lobbyID, playerID, creation
+            - Runs the arena code based on the lobbyID for the players once both players have committed their creations
+            - Output: waiting for next player state or outcome proof
+        - `/games/commit` (POST)
+            - Input: proof from play
+            - Verifies the proof and extracts the final outcome from the receipt
+            - Updates the DB with the outcome
+            - Output: success/fail
+        - `/games/` (GET)
+            - Returns all the finished games
+    - Talks with the Risc Zero ZK VM to generate and verify proofs
+    - Talks with database to store and provide the global records
+- `methods/`
+    - The code that will run inside the RiscZero ZK VM
+    - Will be public so that people can verify it using the hash of it
+    - In the initial case, it will be the first public auto battler area we build
+
+### Database
+- Initially, for the database we are going to use Google Firestore
+    - Anyone will be able to read from this database
+    - And there will be an endpoint which anyone can submit a valid proof to update the database
+- We understand this is a point of centralization, as we as the host of this database, could do anything we want with it. However, we chose to do this because the verifier for Risc Zero is currently in Rust and popular blockchains do not support it. Eventually, all these global records would be stored on-chain with contracts to read and write to them.
+- DB Schema
+    - Users collection
+        - User Document
+            - user_ID
+    - Games Collection
+        - Game Document
+            - playerA_ID
+            - playerB_ID
+            - creationA_Hash
+            - creationB_Hash
+            - arena_hash
+            - winnerCreation_Hash
+            - state: [playerA_TURN, playerB_TURN, playing, complete]
+            - lobby_id
+    - Lobby Collection
+        - Lobby Document
+            - lobby_ID
+            - playerA_ID
+            - playerB_ID
+    - TODO: Lobby state + pending games state for each player
+
+
+### TODO
+- How do users move creations between browsers? Or if they clear data in browser, do they loose it all?
+- Brute force hash table problem
---
+++ server/src/main.rs
@@ -0,0 +1,119 @@
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    response::IntoResponse,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+
+use methods::{MULTIPLY_ID, MULTIPLY_PATH};
+use risc0_zkvm::host::Prover;
+use risc0_zkvm::serde::{from_slice, to_vec};
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct FactorsInput {
+    a: u64,
+    b: u64
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct Receipt {
+    journal: Vec<u8>,
+    seal: Vec<u32>,
+}
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/prove", post(prove_factors));
+
+    // run our app with hyper
+    // `axum::Server` is a re-export of `hyper::Server`
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    tracing::info!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, Tenet!"
+}
+
+async fn prove_factors(
+    // this argument tells axum to parse the request body
+    Json(payload): Json<FactorsInput>,
+) -> impl IntoResponse {
+    println!("prove_factors");
+
+    let out = match do_factors_proof(payload) {
+        Ok(receipt) => receipt,
+        Err(_e) => {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                String::from("bad proof load"),
+            )
+        }
+    };
+    (StatusCode::OK, out)
+}
+
+fn do_factors_proof(payload: FactorsInput) -> Result<String, risc0_zkvm::host::Exception>  {
+    // Pick two numbers
+    let a: u64 = payload.a;
+    let b: u64 = payload.b;
+
+    // Multiply them inside the ZKP
+    // First, we make the prover, loading the 'multiply' method
+    let multiply_src = std::fs::read(MULTIPLY_PATH)
+        .expect("Method code should be present at the specified path; did you use the correct *_PATH constant?");
+    let mut prover = Prover::new(&multiply_src, MULTIPLY_ID).expect(
+        "Prover should be constructed from valid method source code and corresponding method ID",
+    );
+
+    // Next we send a & b to the guest
+    prover.add_input(to_vec(&a).unwrap().as_slice()).unwrap();
+    prover.add_input(to_vec(&b).unwrap().as_slice()).unwrap();
+    // Run prover & generate receipt
+    let receipt = prover.run()
+        .expect("Valid code should be provable if it doesn't overflow the cycle limit. See `embed_methods_with_options` for information on adjusting maximum cycle count.");
+
+    println!("Proof done!");
+
+    // // Extract journal of receipt (i.e. output c, where c = a * b)
+    // let c: u64 = from_slice(
+    //     &receipt
+    //         .get_journal_vec()
+    //         .expect("Journal should be available for valid receipts"),
+    // )
+    // .expect("Journal output should deserialize into the same types (& order) that it was written");
+
+    // // Print an assertion
+    // println!("I know the factors of {}, and I can prove it!", c);
+
+    // // Here is where one would send 'receipt' over the network...
+
+    // // Verify receipt, panic if it's wrong
+    // receipt.verify(MULTIPLY_ID).expect(
+    //     "Code you have proven should successfully verify; did you specify the correct method ID?",
+    // );
+
+
+    let receipt = Receipt {
+        journal: receipt.get_journal().unwrap().to_vec(),
+        seal: receipt.get_seal().unwrap().to_vec(),
+    };
+
+    Ok(base64::encode(bincode::serialize(&receipt).unwrap()))
+}
