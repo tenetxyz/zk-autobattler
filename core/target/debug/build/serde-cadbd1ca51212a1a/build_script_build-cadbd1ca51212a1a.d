2671a5903251c30d346147b8e351b92b5b8f3b94
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2023-02-25 16:26:09 -0700

    update gitignore in core/

--- core/target/debug/build/serde-cadbd1ca51212a1a/build_script_build-cadbd1ca51212a1a.d
+++
@@ -1,5 +0,0 @@
-/Users/dhvani/Documents/Tenet/zk-autobattler/core/target/debug/build/serde-cadbd1ca51212a1a/build_script_build-cadbd1ca51212a1a: /Users/dhvani/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.152/build.rs
-
-/Users/dhvani/Documents/Tenet/zk-autobattler/core/target/debug/build/serde-cadbd1ca51212a1a/build_script_build-cadbd1ca51212a1a.d: /Users/dhvani/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.152/build.rs
-
-/Users/dhvani/.cargo/registry/src/github.com-1ecc6299db9ec823/serde-1.0.152/build.rs:
--- core/target/debug/deps/tenet_core-383e5d56ed14551d.d
+++
@@ -1,5 +0,0 @@
-/Users/dhvani/Documents/Tenet/zk-autobattler/core/target/debug/deps/tenet_core-383e5d56ed14551d.rmeta: src/lib.rs
-
-/Users/dhvani/Documents/Tenet/zk-autobattler/core/target/debug/deps/tenet_core-383e5d56ed14551d.d: src/lib.rs
-
-src/lib.rs:
